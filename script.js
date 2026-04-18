439246057e6c40463b6eb523d102f2f43e939093
trail-fun <mik@bronze.ocn.ne.jp>
2026-04-18 22:11:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ runners.forEach((file, index) => {
         map.fitBounds(gpx.getBounds());
 
     // 1. 軌跡の全座標 (緯度経度配列) を取得
-　  const lat = e.point.getLatLng().lat; // 緯度
+　  const lat = gpx.point.getLatLng().lat; // 緯度
 //    const points = gpx_parse_gpx_data(t,e); 
 //    console.log('Points:', points.name); // [{lat: ..., lon: ..., ele: ...}, ...]
         
