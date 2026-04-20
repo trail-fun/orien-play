cfece4d37e0c534c839aae24533689b132d58fe5
trail-fun <mik@bronze.ocn.ne.jp>
2026-04-20 22:10:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ runners.forEach((file, index) => {
 //    });
 const time = e.element.getElementsByTagName('time')[0].textContent;
 const objdata = e.element.getElementsByTagName('time');
-console.log("times:",objdata)
+console.log("times:",objdata);
         //NG const objData=JSON.parse(e);
 //NG console.log("test:",objData.time);
         //　  const lat = gpx.point.getLatLng().lat; // 緯度
